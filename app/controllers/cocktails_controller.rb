57e52dc0c7c6fb44fe19c30de3bf9e5b598981b0
Alina Gotcherian <alina.gotcherian@gmail.com>
2019-05-17 09:53:03 +0200

    automatic save

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,20 @@
 class CocktailsController < ApplicationController
   def name
   end
+
+  def index
+    @cocktails = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params[:cocktail])
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,15 @@
 class DosesController < ApplicationController
   def description
   end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(params[:dose])
+  end
+
+  def destroy
+  end
 end
